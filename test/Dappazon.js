c337c7e23eac750fe1f80dd042773a1d00acf5ed
Antesh <anteshkumar2003@gmail.com>
2024-10-26 22:33:15 +0530

    finished creating smart contracts and UI components

--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,130 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
+const ID = 1;
+const NAME = "Shoes";
+const CATEGORY = "Clothing";
+const IMAGE = "https://i.pravatar.cc";
+const COST = tokens(1);
+const RATING = 5;
+const STOCK = 10;
 
 describe("Dappazon", () => {
+  let dappazon, deployer, buyer;
 
-})
+  beforeEach(async () => {
+    //Setup Accounts
+    [deployer, buyer] = await ethers.getSigners();
+
+    //Deploy Contracts
+    const Dappazon = await ethers.getContractFactory("Dappazon");
+    dappazon = await Dappazon.deploy();
+  });
+
+  describe("Deployment", () => {
+    it("Sets the owner", async () => {
+      expect(await dappazon.owner()).to.equal(deployer.address);
+    });
+  });
+
+  describe("Listing", () => {
+    let transaction;
+
+    beforeEach(async () => {
+      transaction = await dappazon
+        .connect(deployer)
+        .list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK);
+      await transaction.wait();
+    });
+
+    it("Returns the Item attributes", async () => {
+      const item = await dappazon.items(1);
+      expect(item.id).to.equal(ID);
+      expect(item.name).to.equal(NAME);
+      expect(item.category).to.equal(CATEGORY);
+      expect(item.image).to.equal(IMAGE);
+      expect(item.cost).to.equal(COST);
+      expect(item.rating).to.equal(RATING);
+      expect(item.stock).to.equal(STOCK);
+    });
+
+    it("Emits a List event", async () => {
+      await expect(transaction).to.emit(dappazon, "List");
+    });
+  });
+
+  describe("Buying", () => {
+    let transaction;
+
+    beforeEach(async () => {
+      //List an Item
+      transaction = await dappazon
+        .connect(deployer)
+        .list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK);
+      await transaction.wait();
+
+      //Buy an Item
+      transaction = await dappazon.connect(buyer).buy(ID, { value: COST });
+      // await transaction.wait();
+    });
+
+    it("Updates the owner's balance", async () => {
+      const result = await ethers.provider.getBalance(deployer.address);
+      expect(result).to.greaterThanOrEqual(COST);
+    });
+
+    it("Updates the buyer's order count", async () => {
+      const result = await dappazon.orderCount(buyer.address);
+      expect(result).to.equal(1);
+    });
+
+    it("Adds the order", async () => {
+      const order = await dappazon.orders(buyer.address, 1);
+      // expect(order.time).to.be.greaterThan(0)
+      expect(order.item.name).to.equal(NAME);
+    });
+
+    it("Updates the contract balance", async () => {
+      const result = await ethers.provider.getBalance(dappazon.address);
+      expect(result).to.equal(COST);
+    });
+
+    it("Emits a Buy event", () => {
+      expect(transaction).to.emit(dappazon, "Buy");
+    });
+  });
+
+  describe("Withdrawing", () => {
+    let balanceBefore;
+    beforeEach(async () => {
+      //List an Item
+      let transaction = await dappazon
+        .connect(deployer)
+        .list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK);
+      await transaction.wait();
+
+      //Buy an Item
+      transaction = await dappazon.connect(buyer).buy(ID, { value: COST });
+      await transaction.wait();
+
+      //Get deployer's balance before
+      balanceBefore = await ethers.provider.getBalance(deployer.address);
+
+      //Withdraw
+      transaction = await dappazon.connect(deployer).withdraw();
+      await transaction.wait();
+    });
+
+    it("Updates the owner's balance", async () => {
+      const balanceAfter = await ethers.provider.getBalance(deployer.address);
+      expect(balanceAfter).to.be.greaterThan(balanceBefore);
+    });
+
+    it("Updates the contract balance", async () => {
+      const balance = await ethers.provider.getBalance(dappazon.address);
+      expect(balance).to.equal(0);
+    });
+  });
+});
